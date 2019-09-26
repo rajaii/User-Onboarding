ca38342e1c3cd85aa8c09b346145951d8e871ee6
Ali Rajaii <rajaii_a@yahoo.com>
2019-09-26 19:39:40 -0400

    MVP

---
+++ userform/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormikUserForm from './components/Form.js';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikUserForm/>
+    </div>
+  );
+}
+
+export default App;
